ab76732a43bcb2d6140dbb8e43cd262c16396c26
Schmoller <steven.schmoll@gmail.com>
2012-08-29 01:54:27 +1000

    Basic stuff

---
+++ SkyQuest/src/au/com/mineauz/SkyQuest/SkyQuestPlugin.java
@@ -0,0 +1,18 @@
+package au.com.mineauz.SkyQuest;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SkyQuestPlugin extends JavaPlugin 
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
