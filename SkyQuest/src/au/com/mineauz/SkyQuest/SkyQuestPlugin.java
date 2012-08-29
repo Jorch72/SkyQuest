46488746485330461de34f24a7a1e1a5ff656a67
Razz0991 <paul.dav.1991@gmail.com>
2012-08-29 10:39:50 +1000

    Set up commands and permissions
    More basic setup stuff

--- SkyQuest/src/au/com/mineauz/SkyQuest/SkyQuestPlugin.java
+++ SkyQuest/src/au/com/mineauz/SkyQuest/SkyQuestPlugin.java
@@ -1,5 +1,9 @@
 package au.com.mineauz.SkyQuest;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SkyQuestPlugin extends JavaPlugin 
@@ -8,11 +12,43 @@ public class SkyQuestPlugin extends JavaPlugin
 	public void onEnable()
 	{
 		
+		getLogger().info(getDescription().getVersion() + " successfully enabled!");
 	}
 	
 	@Override
 	public void onDisable()
 	{
 		
+		getLogger().info("successfully disabled!");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String cmdlabel, String[] args){
+		Player player = null;
+		if(sender instanceof Player){
+			player = (Player) sender;
+		}
+		
+		if(cmd.getName().equalsIgnoreCase("skyquest")){
+			if(args.length == 1 && args[0].equalsIgnoreCase("help")){
+				if(player != null && player.hasPermission("skyquest.skyquest") && player.hasPermission("skyquest.help")){
+					player.sendMessage(ChatColor.LIGHT_PURPLE + "Sky Quest");
+					player.sendMessage(ChatColor.GRAY + "Version: " + getDescription().getVersion());
+					player.sendMessage(ChatColor.GRAY + "By: " + getDescription().getAuthors().get(0));
+				}
+				else{
+					getLogger().info("Sky Quest");
+					getLogger().info("Version: " + getDescription().getVersion());
+					getLogger().info("By: " + getDescription().getAuthors().get(0));
+				}
+				return true;
+			}
+			else{
+				player.sendMessage(ChatColor.RED + "Invalid command! Type '/skyquest help' for help");
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
