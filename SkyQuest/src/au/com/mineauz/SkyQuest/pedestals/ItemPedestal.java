75b6bfaacbc7e0dfaf00885b22be1d4d4bb3c9cb
Razz0991 <paul.dav.1991@gmail.com>
2012-09-16 18:43:45 +1000

    Changes on Item Pedestals

--- SkyQuest/src/au/com/mineauz/SkyQuest/pedestals/ItemPedestal.java
+++ SkyQuest/src/au/com/mineauz/SkyQuest/pedestals/ItemPedestal.java
@@ -1,5 +1,9 @@
 package au.com.mineauz.SkyQuest.pedestals;
 
+import net.minecraft.server.NBTTagCompound;
+import net.minecraft.server.NBTTagList;
+import net.minecraft.server.NBTTagString;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -11,8 +15,11 @@ import au.com.mineauz.SkyQuest.Util;
 
 public class ItemPedestal extends PedestalBase{
 	
+	NBTTagList plyList;
+	
 	public ItemPedestal(Location pedestalLocation, ItemStack item){
 		super(new ItemStack(Material.EMERALD_BLOCK), item, pedestalLocation);
+		plyList = new NBTTagList();
 	}
 	public ItemPedestal(){
 		super();
@@ -21,13 +28,23 @@ public class ItemPedestal extends PedestalBase{
 	@Override
 	protected void onPlayerActivatePedestal(Player player) {
 		MagicBook book = Util.getMagicBookFor(player);
-		if(book == null)
-		{
-			player.sendMessage(ChatColor.RED + "You need to be carrying your magic book to activate this pedestal");
-			return;
+		boolean hasplayer = false;
+		for(int i = 0; i < plyList.size(); i++){
+			if(plyList.get(i).getName().equalsIgnoreCase(player.getName())){
+				hasplayer = true;
+			}
 		}
-		else{
-			player.getInventory().addItem(getItemStack());
+		if(!hasplayer){
+			if(book == null)
+			{
+				player.sendMessage(ChatColor.RED + "You need to be carrying your magic book to activate this pedestal");
+				return;
+			}
+			else{
+				player.getInventory().addItem(getItemStack());
+				plyList.add(new NBTTagString(player.getName(), player.getName()));
+				player.sendMessage("You have found a " + this.getItemStack().getType().toString().replace("_", " ").toLowerCase());
+			}
 		}
 	}
 
@@ -35,5 +52,17 @@ public class ItemPedestal extends PedestalBase{
 	protected void onPedestalDestroyed() {
 		Pedestals.removePedestal(getLocation());
 	}
-
+	
+	@Override
+	public void writeToNBT(NBTTagCompound root){
+		super.writeToNBT(root);
+		root.set("PlyList", plyList);
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound root){
+		super.readFromNBT(root);
+		
+		plyList = root.getList("PlyList");
+	}
 }
--- SkyQuest/src/au/com/mineauz/SkyQuest/pedestals/PedestalBase.java
+++ SkyQuest/src/au/com/mineauz/SkyQuest/pedestals/PedestalBase.java
@@ -100,7 +100,7 @@ public abstract class PedestalBase
 	}
 	
 	public ItemStack getItemStack(){
-		return mHoverItemTemplate;
+		return mHoverItemTemplate.clone();
 	}
 	
 	/**
